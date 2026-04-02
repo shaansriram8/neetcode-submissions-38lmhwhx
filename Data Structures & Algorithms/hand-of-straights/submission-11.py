1671b41bf469b16f99f3cb0f3ec6384fcfb27464
Shaan Sriram <shaansriram8@gmail.com>
2026-04-01 23:41:27 -0400

    Add: hand-of-straights - submission-11

---
+++ Data Structures & Algorithms/hand-of-straights/submission-11.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+
+        if len(hand) % groupSize != 0:
+            return False
+
+        hashmap = {}
+
+        hand.sort()
+        print(hand)
+        for val in hand:
+            hashmap[val] = hashmap.get(val, 0) + 1
+
+        stop = len(hand) // groupSize
+
+        for key, val in hashmap.items():
+            for j in range(hashmap[key]):
+                print("outer", key)
+                for i in range(groupSize):
+                    print(key+i)
+                    if key + i not in hashmap or hashmap[key+i] == 0:
+                        return False
+                    hashmap[key+i] -= 1
+                    print("freq", hashmap[key+i])
+        return True
+            
+            
+
+        
+
+        
+
+
+
+
