85ffcb6f77e668c34e02fa759fe2e374f18edb5c
Shaan Sriram <shaansriram8@gmail.com>
2026-04-29 20:56:15 -0400

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.py
@@ -0,0 +1,28 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        out = []
+        hashmap = {}
+        for string in strs:
+            key = [0] * 26
+            for char in string:
+                key[ord(char)-ord('a')] += 1
+            if tuple(key) not in hashmap:
+                hashmap[tuple(key)] = [string]
+            else:
+                hashmap[tuple(key)].append(string)
+        
+        for key, val in hashmap.items():
+            out.append(val)
+        return out
+
+            
+        
+
+
+
+            
+            
+            
+
+         
+        
