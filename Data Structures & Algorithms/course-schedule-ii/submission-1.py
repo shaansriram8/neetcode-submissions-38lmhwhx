1aa57aa65eccb961c74b693d2832ec3e45bceb99
Shaan Sriram <shaansriram8@gmail.com>
2026-06-28 19:40:18 -0400

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        out = []
+        n = numCourses
+        degree = [0] * n
+        adj = [[] for _ in range(n)]
+
+        for a, b in prerequisites:
+            adj[b].append(a)
+            degree[a] += 1
+        
+        queue = deque()
+        for ind, val in enumerate(degree):
+            if val == 0:
+                queue.append(ind)
+        
+        while queue:
+            node = queue.popleft()
+            out.append(node)
+            for neighbor in adj[node]:
+                degree[neighbor] -=1
+                if degree[neighbor] == 0:
+                    queue.append(neighbor)
+        return [] if len(out) != n else out
