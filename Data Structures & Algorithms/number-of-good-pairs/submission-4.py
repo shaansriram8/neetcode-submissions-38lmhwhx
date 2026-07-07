a4d1230a98231a084f9f67cbfeb4dfe220f79b7e
Shaan Sriram <shaansriram8@gmail.com>
2026-07-06 23:20:42 -0400

    Add: number-of-good-pairs - submission-4

---
+++ Data Structures & Algorithms/number-of-good-pairs/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        hashmap = defaultdict(list)
+        count = 0
+        for ind, num in enumerate(nums):
+            hashmap[num].append([ind, num])
+        for key, val in hashmap.items():
+            for pair in val:
+                j = 1
+                while j < len(val):
+                    if val[j][0] > pair[0]:
+                        count +=1
+                    j+=1
+        return count
+                    
+
+
+
+
+        
